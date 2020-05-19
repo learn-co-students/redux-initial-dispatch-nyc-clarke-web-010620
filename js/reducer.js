e4ab4f446ff220a6b24b9dbe9a1e45e7cd28e518
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-05-19 18:04:42 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,9 +1,15 @@
-let state = {count: 0};
+let state
+let action = {type: 'INCREASE_COUNT'}
+let action1 = {type: 'DECREASE_COUNT'}
 
-function changeState(state, action){
+function changeState(state = { count: 0 }, action){
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
+      case 'DECREASE_COUNT':
+        return {count: state.count - 1}
+      case 'ZERO_COUNT':
+        return {count: 0}
       default:
         return state;
     }
@@ -12,8 +18,28 @@ function changeState(state, action){
 function dispatch(action){
     state = changeState(state, action)
     render()
+    console.log(state.count)
 }
 
 function render(){
     document.body.textContent = state.count
 }
+
+// Functions To Execute
+
+dispatch({ type: '@@INIT' })
+dispatch({ type: 'INCREASE_COUNT' })
+dispatch({ type: 'INCREASE_COUNT' })
+dispatch({ type: 'INCREASE_COUNT' })
+dispatch({ type: '@@INIT' })
+dispatch({ type: 'DECREASE_COUNT' })
+dispatch({ type: 'ZERO_COUNT' })
+dispatch({ type: 'INCREASE_COUNT' })
+dispatch({ type: 'INCREASE_COUNT' })
+dispatch({ type: 'DECREASE_COUNT' })
+dispatch({ type: 'DECREASE_COUNT' })
+dispatch({ type: 'DECREASE_COUNT' })
+dispatch({ type: 'DECREASE_COUNT' })
+dispatch({ type: 'DECREASE_COUNT' })
+dispatch({ type: 'DECREASE_COUNT' })
+
